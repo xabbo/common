2d04c2c2113874bc34f5ef692b2223ae8f998c2b
b7c <58299468+b7c@users.noreply.github.com>
2021-05-10 13:28:02 +1200

    add SendAsync with implicit destination based on header to IInterceptor

--- Xabbo.Common/Interceptor/IInterceptor.cs
+++ Xabbo.Common/Interceptor/IInterceptor.cs
@@ -59,6 +59,18 @@ namespace Xabbo.Interceptor
         event EventHandler<InterceptArgs>? Intercepted;
 
         /// <summary>
+        /// Sends a packet with the specified header and values
+        /// to either the client or server, depending on the destination of the header.
+        /// </summary>
+        Task SendAsync(Header header, params object[] values);
+
+        /// <summary>
+        /// Sends the specified packet to either the client or server,
+        /// depending on the destination of the packet header.
+        /// </summary>
+        Task SendAsync(IReadOnlyPacket packet);
+
+        /// <summary>
         /// Sends a packet with the specified header and values to the server.
         /// </summary>
         Task SendToServerAsync(Header header, params object[] values);
