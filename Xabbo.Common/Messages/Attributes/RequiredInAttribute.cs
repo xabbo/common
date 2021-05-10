c7d0872ff6f57b1a7079b0cca736076e17e600e4
b7c <58299468+b7c@users.noreply.github.com>
2021-05-10 13:27:10 +1200

    add required identifiers attribute

---
+++ Xabbo.Common/Messages/Attributes/RequiredInAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xabbo.Messages
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequiredInAttribute : IdentifiersAttribute
+    {
+        public RequiredInAttribute(params string[] identifiers)
+          : base(Destination.Client, identifiers)
+        { }
+    }
+}
---
+++ Xabbo.Common/Messages/Attributes/RequiredOutAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xabbo.Messages
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequiredOutAttribute : IdentifiersAttribute
+    {
+        public RequiredOutAttribute(params string[] identifiers)
+          : base(Destination.Server, identifiers)
+        { }
+    }
+}
