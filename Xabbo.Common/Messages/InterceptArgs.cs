96322f1ebe28c3a82aadc22cae929a5869954742
b7c <58299468+b7c@users.noreply.github.com>
2021-05-13 22:47:11 +1200

    add OriginalPacket to InterceptArgs

--- Xabbo.Common/Messages/InterceptArgs.cs
+++ Xabbo.Common/Messages/InterceptArgs.cs
@@ -7,24 +7,23 @@ namespace Xabbo.Messages
     {
         private bool _disposed;
 
-        private readonly Header _originalHeader = Header.Unknown;
         private readonly IMemoryOwner<byte> _originalDataOwner;
-        private readonly ReadOnlyMemory<byte> _originalData;
 
         public DateTime Timestamp { get; }
         public Destination Destination { get; }
         public ClientType Client { get; }
         public int Step { get; }
         public IPacket Packet { get; }
+        public IReadOnlyPacket OriginalPacket { get; }
 
         public bool IsIncoming => Destination == Destination.Client;
         public bool IsOutgoing => Destination == Destination.Server;
 
         public bool IsBlocked { get; private set; }
         public bool IsModified =>
-            Packet.Header != _originalHeader ||
-            Packet.Length != _originalData.Length ||
-            !Packet.GetBuffer().Span.SequenceEqual(_originalData.Span);
+            Packet.Header != OriginalPacket.Header ||
+            Packet.Length != OriginalPacket.Length ||
+            !Packet.GetBuffer().Span.SequenceEqual(OriginalPacket.GetBuffer().Span);
 
         public InterceptArgs(Destination destination, ClientType client, int step, IPacket packet)
         {
@@ -34,19 +33,17 @@ namespace Xabbo.Messages
             Step = step;
             Packet = packet;
 
-            _originalHeader = packet.Header;
             _originalDataOwner = MemoryPool<byte>.Shared.Rent(packet.Length);
-            _originalData = _originalDataOwner.Memory[0..packet.Length];
-
             packet.CopyTo(_originalDataOwner.Memory.Span);
+
+            OriginalPacket = new Packet(
+                client,
+                packet.Header,
+                _originalDataOwner.Memory[0..packet.Length]
+            );
         }
 
         public void Block() => IsBlocked = true;
-
-        public IPacket GetOriginalPacket()
-        {
-            return new Packet(_originalData.Span) { Header = _originalHeader };
-        }
 
         protected virtual void Dispose(bool disposing)
         {
