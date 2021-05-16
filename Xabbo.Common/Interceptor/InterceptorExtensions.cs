ba3d19c859a04354343b285e9c3948e799fcd7b3
b7c <58299468+b7c@users.noreply.github.com>
2021-05-16 17:11:51 +1200

    add documentation, overloads without CancellationToken

--- Xabbo.Common/Interceptor/InterceptorExtensions.cs
+++ Xabbo.Common/Interceptor/InterceptorExtensions.cs
@@ -10,11 +10,16 @@ namespace Xabbo.Interceptor
 {
     public static class InterceptorExtensions
     {
+        /// <summary>
+        /// Asynchronously captures the first incoming packet with one of the specified target headers.
+        /// Throws an <see cref="OperationCanceledException"/> if no matching packets are received
+        /// after the specified <paramref name="timeout"/> in milliseconds, or the specified 
+        /// <see cref="CancellationToken"/> is cancelled.
+        /// </summary>
         public static Task<IPacket> ReceiveAsync(this IInterceptor interceptor,
-            int timeout, CancellationToken cancellationToken,
-            params Header[] targetHeaders)
+            int timeout, CancellationToken cancellationToken, params Header[] targetHeaders)
         {
-            if (targetHeaders.Length == 0)
+            if (targetHeaders is null || targetHeaders.Length == 0)
                 throw new ArgumentException("At least one target header must be specified.");
 
             if (targetHeaders.Any(header => header.Destination == Destination.Client))
@@ -24,11 +29,27 @@ namespace Xabbo.Interceptor
                 .ExecuteAsync(timeout, cancellationToken);
         }
 
-        public static Task<IPacket> CaptureOutAsync(this IInterceptor interceptor,
-            int timeout, CancellationToken cancellationToken,
-            params Header[] targetHeaders)
+        /// <summary>
+        /// Asynchronously captures the first incoming packet with one of the specified target headers.
+        /// Throws an <see cref="OperationCanceledException"/> if no matching packets are received
+        /// after the specified <paramref name="timeout"/> in milliseconds.
+        /// </summary>
+        public static Task<IPacket> ReceiveAsync(this IInterceptor interceptor,
+            int timeout, params Header[] targetHeaders)
         {
-            if (targetHeaders.Length == 0)
+            return ReceiveAsync(interceptor, timeout, CancellationToken.None, targetHeaders);
+        }
+
+        /// <summary>
+        /// Asynchronously captures the first outgoing packet with one of the specified target headers.
+        /// Throws an <see cref="OperationCanceledException"/> if no matching packets are sent
+        /// after the specified <paramref name="timeout"/> in milliseconds, or the specified 
+        /// <see cref="CancellationToken"/> is cancelled.
+        /// </summary>
+        public static Task<IPacket> CaptureOutAsync(this IInterceptor interceptor,
+            int timeout, CancellationToken cancellationToken, params Header[] targetHeaders)
+        {
+            if (targetHeaders is null || targetHeaders.Length == 0)
                 throw new ArgumentException("At least one target header must be specified.");
 
             if (targetHeaders.Any(header => header.Destination == Destination.Client))
@@ -36,6 +57,17 @@ namespace Xabbo.Interceptor
 
             return new CaptureMessageTask(interceptor, Destination.Server, false, targetHeaders)
                 .ExecuteAsync(timeout, cancellationToken);
+        }
+
+        /// <summary>
+        /// Asynchronously captures the first outgoing packet with one of the specified target headers.
+        /// Throws an <see cref="OperationCanceledException"/> if no matching packets are sent
+        /// after the specified <paramref name="timeout"/> in milliseconds.
+        /// </summary>
+        public static Task<IPacket> CaptureOutAsync(this IInterceptor interceptor,
+            int timeout, params Header[] targetHeaders)
+        {
+            return CaptureOutAsync(interceptor, timeout, CancellationToken.None, targetHeaders);
         }
     }
 }
