15219d7e1312dbb1995aeeb9717877aefbc680ca
b7c <58299468+b7c@users.noreply.github.com>
2021-07-27 00:10:11 +1200

    change direction enum to flags

--- Xabbo.Common/Messages/Direction.cs
+++ Xabbo.Common/Messages/Direction.cs
@@ -2,10 +2,12 @@
 
 namespace Xabbo.Messages
 {
+    [Flags]
     public enum Direction
     {
-        Unknown,
-        Incoming,
-        Outgoing
+        Unknown = 0,
+        Incoming = 1,
+        Outgoing = 2,
+        Both = Incoming | Outgoing
     }
 }
