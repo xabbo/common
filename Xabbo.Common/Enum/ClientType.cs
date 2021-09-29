87405898592ea8620cbb0eae7336108b84d576c4
b7c <58299468+b7c@users.noreply.github.com>
2021-09-30 01:47:34 +1300

    Add documentation

--- Xabbo.Common/Enum/ClientType.cs
+++ Xabbo.Common/Enum/ClientType.cs
@@ -2,11 +2,23 @@
 
 namespace Xabbo
 {
+    /// <summary>
+    /// Represents a type of game client.
+    /// </summary>
     [Flags]
     public enum ClientType
     {
-        Unknown = 0,
-        Flash = 1,
-        Unity = 2
+        /// <summary>
+        /// Represents an unknown client type.
+        /// </summary>
+        Unknown,
+        /// <summary>
+        /// Represents the Flash client.
+        /// </summary>
+        Flash = 1 << 0,
+        /// <summary>
+        /// Represents the Unity client.
+        /// </summary>
+        Unity = 1 << 2
     }
 }
