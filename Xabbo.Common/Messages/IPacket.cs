b6cdb7c3ae0252148db1b431b95a04aae259c073
b7c <58299468+b7c@users.noreply.github.com>
2022-06-18 01:36:26 +1200

    update packet interfaces

--- Xabbo.Common/Messages/IPacket.cs
+++ Xabbo.Common/Messages/IPacket.cs
@@ -1,155 +1,152 @@
 ﻿using System;
 
-namespace Xabbo.Messages
+namespace Xabbo.Messages;
+
+/// <summary>
+/// Represents a writable packet of binary data with a message header.
+/// </summary>
+public interface IPacket : IReadOnlyPacket
 {
-    public interface IPacket : IReadOnlyPacket
-    {
-        /// <summary>
-        /// Gets or sets the message header of the packet.
-        /// </summary>
-        new Header Header { get; set; }
+    /// <summary>
+    /// Gets or sets the message header of the packet.
+    /// </summary>
+    new Header Header { get; set; }
 
-        /// <summary>
-        /// Writes the composable object to the packet.
-        /// </summary>
-        IPacket Write(IComposable composable);
+    /// <summary>
+    /// Gets the underlying memory of the packet's data.
+    /// </summary>
+    new Memory<byte> Buffer { get; }
 
-        /// <summary>
-        /// Writes a boolean to the current position in the packet.
-        /// </summary>
-        IPacket WriteBool(bool value);
+    /// <summary>
+    /// Writes a composable object to the packet.
+    /// </summary>
+    IPacket Write(IComposable composable);
 
-        /// <summary>
-        /// Writes a boolean to the specified position in the packet.
-        /// </summary>
-        IPacket WriteBool(bool value, int position);
+    /// <summary>
+    /// Writes a boolean to the current position in the packet.
+    /// </summary>
+    IPacket WriteBool(bool value);
 
-        /// <summary>
-        /// Writes a byte to the current position in the packet.
-        /// </summary>
-        IPacket WriteByte(byte value);
+    /// <summary>
+    /// Writes a boolean to the specified position in the packet.
+    /// </summary>
+    IPacket WriteBool(bool value, int position);
 
-        /// <summary>
-        /// Writes a byte to the specified position in the packet.
-        /// </summary>
-        IPacket WriteByte(byte value, int position);
+    /// <summary>
+    /// Writes a byte to the current position in the packet.
+    /// </summary>
+    IPacket WriteByte(byte value);
 
-        /// <summary>
-        /// Writes a short to the current position in the packet.
-        /// </summary>
-        IPacket WriteShort(short value);
+    /// <summary>
+    /// Writes a byte to the specified position in the packet.
+    /// </summary>
+    IPacket WriteByte(byte value, int position);
 
-        /// <summary>
-        /// Writes a short to the specified position in the packet.
-        /// </summary>
-        IPacket WriteShort(short value, int position);
+    /// <summary>
+    /// Writes a short to the current position in the packet.
+    /// </summary>
+    IPacket WriteShort(short value);
 
-        /// <summary>
-        /// Writes an integer to the current position in the packet.
-        /// </summary>
-        IPacket WriteInt(int value);
+    /// <summary>
+    /// Writes a short to the specified position in the packet.
+    /// </summary>
+    IPacket WriteShort(short value, int position);
 
-        /// <summary>
-        /// Writes an integer to the specified position in the packet.
-        /// </summary>
-        IPacket WriteInt(int value, int position);
+    /// <summary>
+    /// Writes an integer to the current position in the packet.
+    /// </summary>
+    IPacket WriteInt(int value);
 
-        /// <summary>
-        /// Writes a float to the current position in the packet.
-        /// </summary>
-        IPacket WriteFloat(float value);
+    /// <summary>
+    /// Writes an integer to the specified position in the packet.
+    /// </summary>
+    IPacket WriteInt(int value, int position);
 
-        /// <summary>
-        /// Writes a float to the specified position in the packet.
-        /// </summary>
-        IPacket WriteFloat(float value, int position);
+    /// <summary>
+    /// Writes a float to the current position in the packet.
+    /// </summary>
+    IPacket WriteFloat(float value);
 
-        /// <summary>
-        /// Writes a long to the current position in the packet.
-        /// </summary>
-        IPacket WriteLong(long value);
+    /// <summary>
+    /// Writes a float to the specified position in the packet.
+    /// </summary>
+    IPacket WriteFloat(float value, int position);
 
-        /// <summary>
-        /// Writes a long to the specified position in the packet.
-        /// </summary>
-        IPacket WriteLong(long value, int position);
+    /// <summary>
+    /// Writes a long to the current position in the packet.
+    /// </summary>
+    IPacket WriteLong(long value);
 
-        /// <summary>
-        /// Writes a string to the current position in the packet.
-        /// </summary>
-        IPacket WriteString(string value);
+    /// <summary>
+    /// Writes a long to the specified position in the packet.
+    /// </summary>
+    IPacket WriteLong(long value, int position);
 
-        /// <summary>
-        /// Writes a string to the specified position in the packet.
-        /// </summary>
-        IPacket WriteString(string value, int position);
+    /// <summary>
+    /// Writes a string to the current position in the packet.
+    /// </summary>
+    IPacket WriteString(string value);
 
-        /// <summary>
-        /// Writes a float as a string to the current position in the packet.
-        /// </summary>
-        IPacket WriteFloatAsString(float value);
+    /// <summary>
+    /// Writes a string to the specified position in the packet.
+    /// </summary>
+    IPacket WriteString(string value, int position);
 
-        /// <summary>
-        /// Writes a float as a string to the specified position in the packet.
-        /// </summary>
-        IPacket WriteFloatAsString(float value, int position);
+    /// <summary>
+    /// Writes a string representation of a float to the current position in the packet.
+    /// </summary>
+    IPacket WriteFloatAsString(float value);
 
-        /// <summary>
-        /// Writes the specified bytes to the current position in the packet.
-        /// </summary>
-        IPacket WriteBytes(ReadOnlySpan<byte> bytes);
+    /// <summary>
+    /// Writes a string representation of a float to the specified position in the packet.
+    /// </summary>
+    IPacket WriteFloatAsString(float value, int position);
 
-        /// <summary>
-        /// Writes the specified bytes to the specified position in the packet.
-        /// </summary>
-        IPacket WriteBytes(ReadOnlySpan<byte> bytes, int position);
+    /// <summary>
+    /// Writes the specified bytes to the current position in the packet.
+    /// </summary>
+    IPacket WriteBytes(ReadOnlySpan<byte> bytes);
 
-        /// <summary>
-        /// Writes the specified values to the current position in the packet.
-        /// </summary>
-        IPacket WriteValues(params object[] values);
+    /// <summary>
+    /// Writes the specified bytes to the specified position in the packet.
+    /// </summary>
+    IPacket WriteBytes(ReadOnlySpan<byte> bytes, int position);
 
-        /// <summary>
-        /// Replaces values from the current position in the packet.
-        /// </summary>
-        IPacket Replace(params object[] values);
+    /// <summary>
+    /// Writes a short or an int to the current position in the packet, depending on its protocol.
+    /// If the protocol is Unity, writes a short.
+    /// If the protocol is Flash, writes an int.
+    /// Otherwise, throws an exception.
+    /// </summary>
+    IPacket WriteLegacyShort(short value);
 
-        /// <summary>
-        /// Reads a string from the current position in the packet
-        /// and replaces it with the result provided by a transform function.
-        /// </summary>
-        IPacket Replace(Func<string, string> transform);
+    /// <summary>
+    /// Writes a float or a string to the current position in the packet, depending on its protocol.
+    /// If the protocol is Unity, writes a float as 4 bytes.
+    /// If the protocol is Flash, writes a string representation of a float.
+    /// Otherwise, throws an exception.
+    /// </summary>
+    IPacket WriteLegacyFloat(float value);
 
-        /// <summary>
-        /// Replaces values from the specified position in the packet.
-        /// </summary>
-        IPacket ReplaceAt(int position, params object[] values);
+    /// <summary>
+    /// Writes a long or an int to the current position in the packet, depending on its protocol.
+    /// If the protocol is Unity, writes a long.
+    /// If the protocol is Flash, writes an int.
+    /// Otherwise, throws an exception.
+    /// </summary>
+    IPacket WriteLegacyLong(long value);
 
-        /// <summary>
-        /// Reads a string from the specified position in the packet
-        /// and replaces it with the result provided by a transform function.
-        /// </summary>
-        IPacket ReplaceAt(int position, Func<string, string> transform);
+    /// <summary>
+    /// Replaces a string at the current position in the packet.
+    /// Adjusts the packet length and offsets any data after the string if the replacement causes the length to change.
+    /// Throws an exception if a string cannot be read at the current position in the packet.
+    /// </summary>
+    IPacket ReplaceString(string value);
 
-        /// <summary>
-        /// Writes a short if the packet's protocol is Unity,
-        /// an int if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        IPacket WriteLegacyShort(short value);
-
-        /// <summary>
-        /// Writes a float if the packet's protocol is Unity,
-        /// a float (as a string) if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        IPacket WriteLegacyFloat(float value);
-
-        /// <summary>
-        /// Writes a long if the packet's protocol is Unity,
-        /// an int if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        IPacket WriteLegacyLong(long value);
-    }
+    /// <summary>
+    /// Replaces a string at the specified position in the packet.
+    /// Adjusts the packet length and offsets any data after the string if the replacement causes the length to change.
+    /// Throws an exception if a string cannot be read at the specified position in the packet.
+    /// </summary>
+    IPacket ReplaceString(string value, int position);
 }
--- Xabbo.Common/Messages/IReadOnlyPacket.cs
+++ Xabbo.Common/Messages/IReadOnlyPacket.cs
@@ -1,170 +1,169 @@
 ﻿using System;
 
-namespace Xabbo.Messages
+using Xabbo.Common;
+
+namespace Xabbo.Messages;
+
+/// <summary>
+/// Represents a readable packet of binary data with a message header.
+/// </summary>
+public interface IReadOnlyPacket : IDisposable
 {
-    public interface IReadOnlyPacket
-    {
-        /// <summary>
-        /// Gets or sets the protocol hint of the packet.
-        /// </summary>
-        ClientType Protocol { get; set; }
+    /// <summary>
+    /// Gets the client protocol hint of the packet.
+    /// </summary>
+    ClientType Protocol { get; }
 
-        /// <summary>
-        /// Gets the message header of the packet.
-        /// </summary>
-        Header Header { get; }
+    /// <summary>
+    /// Gets the message header of the packet.
+    /// </summary>
+    Header Header { get; }
 
-        /// <summary>
-        /// Gets or sets the current position in the packet.
-        /// </summary>
-        int Position { get; set; }
+    /// <summary>
+    /// Gets or sets the current position in the packet.
+    /// </summary>
+    int Position { get; set; }
 
-        /// <summary>
-        /// Gets the length of the data in the packet.
-        /// </summary>
-        int Length { get; }
+    /// <summary>
+    /// Gets the length of the data in the packet.
+    /// </summary>
+    int Length { get; }
 
-        /// <summary>
-        /// Gets the number of available bytes left in the packet.
-        /// </summary>
-        int Available { get; }
+    /// <summary>
+    /// Gets the number of available bytes left in the packet.
+    /// </summary>
+    int Available { get;  }
 
-        /// <summary>
-        /// Gets the internal data buffer of the packet as <see cref="ReadOnlyMemory{T}"/>.
-        /// </summary>
-        ReadOnlyMemory<byte> GetBuffer();
+    /// <summary>
+    /// Gets the underlying memory of the packet's data.
+    /// </summary>
+    ReadOnlyMemory<byte> Buffer { get; }
 
-        /// <summary>
-        /// Copies the data of the packet into a <see cref="Span{T}"/>.
-        /// </summary>
-        void CopyTo(Span<byte> destination);
+    /// <summary>
+    /// Returns whether a boolean can be read from the current position in the packet.
+    /// </summary>
+    /// <returns><c>true</c> if a byte can be read from the current position in the packet and its value is either <c>0</c> or <c>1</c>.</returns>
+    bool CanReadBool();
 
-        /// <summary>
-        /// Gets if a boolean can be read from the current position in the packet.
-        /// Returns true only if the next available byte is either 0 or 1.
-        /// </summary>
-        bool CanReadBool();
+    /// <summary>
+    /// Returns whether a string can be read from the current position in the packet.
+    /// </summary>
+    bool CanReadString();
 
-        /// <summary>
-        /// Gets if a string can be read from the current position in the packet.
-        /// </summary>
-        bool CanReadString();
+    /// <summary>
+    /// Reads a boolean from the current position in the packet.
+    /// </summary>
+    bool ReadBool();
 
-        /// <summary>
-        /// Reads a boolean from the current position in the packet.
-        /// </summary>
-        bool ReadBool();
+    /// <summary>
+    /// Reads a boolean from the specified position in the packet.
+    /// </summary>
+    bool ReadBool(int position);
 
-        /// <summary>
-        /// Reads a boolean from the specified position in the packet.
-        /// </summary>
-        bool ReadBool(int position);
+    /// <summary>
+    /// Reads a byte from the current position in the packet.
+    /// </summary>
+    byte ReadByte();
 
-        /// <summary>
-        /// Reads a byte from the current position in the packet.
-        /// </summary>
-        byte ReadByte();
+    /// <summary>
+    /// Reads a byte from the specified position in the packet.
+    /// </summary>
+    byte ReadByte(int position);
 
-        /// <summary>
-        /// Reads a byte from the specified position in the packet.
-        /// </summary>
-        byte ReadByte(int position);
+    /// <summary>
+    /// Reads a short from the current position in the packet.
+    /// </summary>
+    short ReadShort();
 
-        /// <summary>
-        /// Reads a short from the current position in the packet.
-        /// </summary>
-        short ReadShort();
+    /// <summary>
+    /// Reads a  from the specified position in the packet.
+    /// </summary>
+    short ReadShort(int position);
 
-        /// <summary>
-        /// Reads a  from the specified position in the packet.
-        /// </summary>
-        short ReadShort(int position);
+    /// <summary>
+    /// Reads an integer from the current position in the packet.
+    /// </summary>
+    int ReadInt();
 
-        /// <summary>
-        /// Reads an integer from the current position in the packet.
-        /// </summary>
-        int ReadInt();
+    /// <summary>
+    /// Reads an integer from the specified position in the packet.
+    /// </summary>
+    int ReadInt(int position);
 
-        /// <summary>
-        /// Reads an integer from the specified position in the packet.
-        /// </summary>
-        int ReadInt(int position);
+    /// <summary>
+    /// Reads a 32-bit floating point number from the current position in the packet.
+    /// </summary>
+    float ReadFloat();
 
-        /// <summary>
-        /// Reads a 32-bit floating point number from the current position in the packet.
-        /// </summary>
-        float ReadFloat();
+    /// <summary>
+    /// Reads a 32-bit floating point number from the specified position in the packet.
+    /// </summary>
+    float ReadFloat(int position);
 
-        /// <summary>
-        /// Reads a 32-bit floating point number from the specified position in the packet.
-        /// </summary>
-        float ReadFloat(int position);
+    /// <summary>
+    /// Reads a long from the current position in the packet.
+    /// </summary>
+    /// <returns></returns>
+    long ReadLong();
 
-        /// <summary>
-        /// Reads a long from the current position in the packet.
-        /// </summary>
-        /// <returns></returns>
-        long ReadLong();
+    /// <summary>
+    /// Reads a long from the specified positions in the packet.
+    /// </summary>
+    long ReadLong(int position);
 
-        /// <summary>
-        /// Reads a long from the specified positions in the packet.
-        /// </summary>
-        long ReadLong(int position);
+    /// <summary>
+    /// Reads a string from the current position in the packet.
+    /// </summary>
+    string ReadString();
 
-        /// <summary>
-        /// Reads a string from the current position in the packet.
-        /// </summary>
-        string ReadString();
+    /// <summary>
+    /// Reads a string from the specified position in the packet.
+    /// </summary>
+    string ReadString(int position);
 
-        /// <summary>
-        /// Reads a string from the specified position in the packet.
-        /// </summary>
-        string ReadString(int position);
+    /// <summary>
+    /// Reads a string from the current position in the packet and parses it into a floating point number.
+    /// </summary>
+    float ReadFloatAsString();
 
-        /// <summary>
-        /// Reads a string from the current position in the packet and parses it into a floating point number.
-        /// </summary>
-        float ReadFloatAsString();
+    /// <summary>
+    /// Reads a string from the specified position in the packet and parses it into a floating point number.
+    /// </summary>
+    float ReadFloatAsString(int position);
 
-        /// <summary>
-        /// Reads a string from the specified position in the packet and parses it into a floating point number.
-        /// </summary>
-        float ReadFloatAsString(int position);
+    /// <summary>
+    /// Copies from the current position in the packet into the <see cref="Span{T}"/>.
+    /// </summary>
+    void Read(Span<byte> buffer);
 
-        /// <summary>
-        /// Copies from the current position in the packet into the <see cref="Span{T}"/>.
-        /// </summary>
-        void ReadBytes(Span<byte> buffer);
+    /// <summary>
+    /// Copies from the specified position in the packet into the <see cref="Span{T}"/>
+    /// </summary>
+    void ReadBytes(Span<byte> buffer, int position);
 
-        /// <summary>
-        /// Copies from the specified position in the packet into the <see cref="Span{T}"/>
-        /// </summary>
-        void ReadBytes(Span<byte> buffer, int position);
+    /// <summary>
+    /// Reads a short if the packet's protocol is Unity,
+    /// an int if it is Flash,
+    /// or throws if unknown.
+    /// </summary>
+    short ReadLegacyShort();
 
-        /// <summary>
-        /// Reads a short if the packet's protocol is Unity,
-        /// an int if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        short ReadLegacyShort();
+    /// <summary>
+    /// Reads a float if the packet's protocol is Unity,
+    /// a float (as a string) if it is Flash,
+    /// or throws if unknown.
+    /// </summary>
+    float ReadLegacyFloat();
 
-        /// <summary>
-        /// Reads a float if the packet's protocol is Unity,
-        /// a float (as a string) if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        float ReadLegacyFloat();
+    /// <summary>
+    /// Reads a long if the packet's protocol is Unity,
+    /// an int if it is Flash,
+    /// or throws if unknown.
+    /// </summary>
+    long ReadLegacyLong();
 
-        /// <summary>
-        /// Reads a long if the packet's protocol is Unity,
-        /// an int if it is Flash,
-        /// or throws if unknown.
-        /// </summary>
-        long ReadLegacyLong();
-
-        /// <summary>
-        /// Creates a copy of this packet.
-        /// </summary>
-        IPacket Clone();
-    }
+    /// <summary>
+    /// Creates a copy of this packet.
+    /// </summary>
+    IPacket Copy();
 }
