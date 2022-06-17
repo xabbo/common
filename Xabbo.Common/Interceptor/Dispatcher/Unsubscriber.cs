733097d4073ec1d54df0d954af759caa58ce1e30
b7c <58299468+b7c@users.noreply.github.com>
2022-06-18 03:54:06 +1200

    add unsubscriber

---
+++ Xabbo.Common/Interceptor/Dispatcher/Unsubscriber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xabbo.Messages;
+
+namespace Xabbo.Interceptor.Dispatcher
+{
+    internal class Unsubscriber : IDisposable
+    {
+        private readonly IInterceptDispatcher _dispatcher;
+        private readonly HeaderSet _headers;
+        private readonly Action<InterceptArgs> _callback;
+
+        private bool _disposed;
+
+        public Unsubscriber(IInterceptDispatcher dispatcher, HeaderSet headers, Action<InterceptArgs> callback)
+        {
+            _dispatcher = dispatcher;
+            _headers = headers;
+            _callback = callback;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            _disposed = true;
+
+            if (disposing)
+            {
+                foreach (Header header in _headers)
+                {
+                    _dispatcher.RemoveIntercept(header, _callback);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+    }
+}
