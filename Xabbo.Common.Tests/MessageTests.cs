f5b7ae66d8cb69695410ad0187ff8bdf591e85c4
b7c <58299468+b7c@users.noreply.github.com>
2022-09-10 21:48:46 +1200

    Add message tests

---
+++ Xabbo.Common.Tests/MessageTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+using Xabbo.Messages;
+
+namespace Xabbo.Common.Tests;
+
+public class MessageTests : IClassFixture<MessagesFixture>
+{
+    private readonly IMessageManager Messages;
+
+    public MessageTests(MessagesFixture messagesFixture)
+    {
+        Messages = messagesFixture.Messages;
+    }
+
+    [Fact(DisplayName = "Unity header value is read correctly")]
+    public void CheckUnityHeaderValue() => Assert.Equal(3, Messages.In["Ok"].GetValue(ClientType.Unity));
+
+    [Fact(DisplayName = "Flash header is merged correctly")]
+    public void CheckFlashHeaderValue() => Assert.Equal(3, Messages.Out.Move.GetValue(ClientType.Flash));
+
+    [Fact(DisplayName = "Property maps to correct header")]
+    public void CheckPropertyMapping() => Assert.Equal(3, Messages.In.Ok.GetValue(ClientType.Unity));
+
+    [Fact(DisplayName = "Property maps to same header as named indexer")]
+    public void CheckPropertyIndexerMapping() => Assert.Equal(Messages.In.Ok, Messages.In["Ok"]);
+
+    [Fact(DisplayName = "Flash / Unity names map to same header")]
+    public void CheckUnityFlashNameMapping() => Assert.Equal(Messages.In["Ok"], Messages.In["AuthenticationOK"]);
+
+    [Fact(DisplayName = "Named indexer is case insensitive")]
+    public void CheckNameIndexerCaseInsensitive() => Assert.Equal(Messages.In["ok"], Messages.In["OK"]);
+
+    [Fact(DisplayName = "Attempting to get value of unresolved header throws correct exception")]
+    public void CheckUnresolvedHeader() => Assert.Throws<UnresolvedHeaderException>(
+        () => Messages.In.DoorOut.GetValue(ClientType.Unity)
+    );
+
+    [Fact(DisplayName = "Attempting to get unknown header throws correct exception")]
+    public void CheckUnknownHeader() => Assert.Throws<UnknownHeaderException>(
+        () => Messages.In["UnknownHeader"]
+    );
+}
---
+++ Xabbo.Common.Tests/MessagesFixture.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xabbo.Messages;
+using Xunit;
+
+namespace Xabbo.Common.Tests;
+
+public class MessagesFixture : IAsyncLifetime
+{
+    public IMessageManager Messages { get; }
+
+    public MessagesFixture()
+    {
+        Messages = new UnifiedMessageManager(@"resources\test_messages.ini") { AutoFetch = false };
+    }
+
+    public async Task InitializeAsync()
+    {
+        await Messages.InitializeAsync(CancellationToken.None);
+
+        Messages.LoadMessages(new ClientMessageInfo[]
+        {
+            new()
+            {
+                Client = ClientType.Flash,
+                Direction = Direction.Incoming,
+                Header = 1,
+                Name = "AuthenticationOK"
+            },
+            new()
+            {
+                Client = ClientType.Flash,
+                Direction = Direction.Outgoing,
+                Header = 2,
+                Name = "InfoRetrieve"
+            },
+            new()
+            {
+                Client = ClientType.Flash,
+                Direction = Direction.Outgoing,
+                Header = 3,
+                Name = "MoveAvatar"
+            }
+        });
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+}
