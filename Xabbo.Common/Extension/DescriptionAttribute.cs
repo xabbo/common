735cced80aedffee42fef809d4f0744f2ca3502e
b7c <58299468+b7c@users.noreply.github.com>
2022-10-03 03:21:26 +1300

    Add extension description attribute

---
+++ Xabbo.Common/Extension/DescriptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Xabbo.Extension;
+
+/// <summary>
+/// Specifies the description of the extension.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class DescriptionAttribute : Attribute
+{
+    /// <summary>
+    /// Gets the description of the extension.
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// Constructs a new <see cref="DescriptionAttribute"/> with the specified description.
+    /// </summary>
+    public DescriptionAttribute(string description)
+    {
+        Description = description;
+    }
+}
