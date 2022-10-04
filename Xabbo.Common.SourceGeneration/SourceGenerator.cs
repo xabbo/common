ff0151593f4c01c97fb333f22230e373d31e2f3d
b7c <58299468+b7c@users.noreply.github.com>
2022-10-05 04:24:47 +1300

    Use Scriban templates for source generator

---
+++ Xabbo.Common.SourceGeneration/SourceGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+using Scriban;
+
+namespace Xabbo.Common.SourceGeneration
+{
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        const int MaxParams = 20;
+
+        private static string GetTemplate(string resourceName)
+        {
+            resourceName = $"Xabbo.Common.SourceGeneration.{resourceName}";
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        private static string RenderTemplate(string resourceName, object model)
+            => Template.Parse(GetTemplate(resourceName)).Render(model);
+
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var model = new { MaxParams };
+
+            context.AddSource("PacketExtensions.g.cs", RenderTemplate("PacketExtensions.sbncs", model));
+            context.AddSource("ConnectionBase.g.cs", RenderTemplate("ConnectionBase.sbncs", model));
+            context.AddSource("ConnectionExtensions.g.cs", RenderTemplate("ConnectionExtensions.sbncs", model));
+        }
+    }
+}
