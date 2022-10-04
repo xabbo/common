7c3c809554b3a2fdb91db149b9de8b267d0e7201
b7c <58299468+b7c@users.noreply.github.com>
2022-10-05 09:26:57 +1300

    Use latest language features

--- Xabbo.Common.SourceGeneration/SourceGenerator.cs
+++ Xabbo.Common.SourceGeneration/SourceGenerator.cs
@@ -6,38 +6,35 @@ using Microsoft.CodeAnalysis.Text;
 
 using Scriban;
 
-namespace Xabbo.Common.SourceGeneration
+namespace Xabbo.Common.SourceGeneration;
+
+[Generator]
+public class SourceGenerator : ISourceGenerator
 {
-    [Generator]
-    public class SourceGenerator : ISourceGenerator
+    const int MaxParams = 20;
+
+    private static string GetTemplate(string resourceName)
     {
-        const int MaxParams = 20;
+        resourceName = $"Xabbo.Common.SourceGeneration.{resourceName}";
+        using Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        using StreamReader sr = new(stream);
+        return sr.ReadToEnd();
+    }
 
-        private static string GetTemplate(string resourceName)
-        {
-            resourceName = $"Xabbo.Common.SourceGeneration.{resourceName}";
-            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
-            using (StreamReader sr = new StreamReader(stream))
-            {
-                return sr.ReadToEnd();
-            }
-        }
+    private static SourceText RenderTemplate(string resourceName, object model)
+    {
+        string renderedTemplate = Template.Parse(GetTemplate(resourceName)).Render(model);
+        return SourceText.From(renderedTemplate, Encoding.UTF8);
+    }
 
-        private static SourceText RenderTemplate(string resourceName, object model)
-        {
-            string renderedTemplate = Template.Parse(GetTemplate(resourceName)).Render(model);
-            return SourceText.From(renderedTemplate, Encoding.UTF8);
-        }
+    public void Initialize(GeneratorInitializationContext context) { }
 
-        public void Initialize(GeneratorInitializationContext context) { }
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var model = new { MaxParams };
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            var model = new { MaxParams };
-
-            context.AddSource("PacketExtensions.g.cs", RenderTemplate("PacketExtensions.sbncs", model));
-            context.AddSource("ConnectionBase.g.cs", RenderTemplate("ConnectionBase.sbncs", model));
-            context.AddSource("ConnectionExtensions.g.cs", RenderTemplate("ConnectionExtensions.sbncs", model));
-        }
+        context.AddSource("PacketExtensions.g.cs", RenderTemplate("PacketExtensions.sbncs", model));
+        context.AddSource("ConnectionBase.g.cs", RenderTemplate("ConnectionBase.sbncs", model));
+        context.AddSource("ConnectionExtensions.g.cs", RenderTemplate("ConnectionExtensions.sbncs", model));
     }
 }
