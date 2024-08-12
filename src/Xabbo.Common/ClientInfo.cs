48a61f268f60f5bb89955fbdd7df89dffdc6f7d9
rob <rob@b7c.io>
2024-08-13 08:38:37 +1200

    add ClientInfo.None

--- src/Xabbo.Common/ClientInfo.cs
+++ src/Xabbo.Common/ClientInfo.cs
@@ -3,4 +3,7 @@
 /// <summary>
 /// Specifies a client type, identifier and version.
 /// </summary>
-public readonly record struct ClientInfo(Client Type, string Identifier, string Version);
+public readonly record struct ClientInfo(Client Type, string Identifier, string Version)
+{
+    public static readonly ClientInfo None = new(Client.None, "", "");
+}
