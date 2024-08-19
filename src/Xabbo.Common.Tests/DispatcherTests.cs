66a5d067c157fb6cc4486e5da3e137ab726ed76b
rob <rob@b7c.io>
2024-08-19 17:43:09 +1200

    update dispatcher tests

--- src/Xabbo.Common.Tests/DispatcherTests.cs
+++ src/Xabbo.Common.Tests/DispatcherTests.cs
@@ -6,6 +6,7 @@ using Xabbo.Interceptor;
 using Xabbo.Extension;
 
 using Xabbo.Common.Tests.Fixtures;
+using Xabbo.Connection;
 
 namespace Xabbo.Common.Tests;
 
@@ -15,7 +16,8 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
     {
         private static Identifier _([CallerMemberName] string? name = null)
             => (ClientType.Flash, Direction.Out, name ?? "");
-        public static Identifier MoveAvatar { get; } = _();
+        public static readonly Identifier MoveAvatar = _();
+        public static readonly Identifier Nonexistent = _();
     }
 
     private static class In
@@ -23,48 +25,84 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
         private static Identifier _([CallerMemberName] string? name = null)
             => (ClientType.Flash, Direction.In, name ?? "");
 
-        public static Identifier Chat { get; } = _();
-        public static Identifier Shout { get; } = _();
-        public static Identifier Whisper { get; } = _();
+        public static readonly Identifier Unresolved = _();
+
+        public static readonly Identifier Chat = _();
+        public static readonly Identifier Shout = _();
+        public static readonly Identifier Whisper = _();
     }
 
-    private readonly MessagesFixture _fixture;
+    private MessagesFixture Fixture { get; }
     private IMessageManager Messages { get; }
     private IMessageDispatcher Dispatcher { get; }
     private IExtension Ext { get; }
 
+    private Mock<IExtension> ExtMock { get; }
+
     public DispatcherTests(MessagesFixture fixture)
     {
-        _fixture = fixture;
+        Fixture = fixture;
         Messages = fixture.Messages;
-        Dispatcher = new MessageDispatcher(Messages);
 
         var mockExt = new Mock<IExtension>();
         mockExt.Setup(x => x.Messages).Returns(Messages);
-        mockExt.Setup(x => x.Dispatcher).Returns(Dispatcher);
+        mockExt.Setup(x => x.Dispatcher).Returns(() => Dispatcher!);
+        ExtMock = mockExt;
         Ext = mockExt.Object;
+
+        Dispatcher = new MessageDispatcher(Ext);
+
+        SimulateConnect();
+    }
+
+    private void SimulateDisconnect()
+    {
+        ExtMock.Raise(x => x.Disconnected += null, null, EventArgs.Empty); // disconnect
+    }
+
+    private void SimulateConnect(ClientType client = ClientType.Flash)
+    {
+        Session session = new(Hotel.None, new Client(client, "", ""));
+        ExtMock.Setup(x => x.Session).Returns(session);
+        ExtMock.Raise(x => x.Connected += null, null, new GameConnectedArgs { Session = session });
+    }
+
+    private void SimulateReconnect(ClientType client = ClientType.Flash)
+    {
+        SimulateDisconnect();
+        SimulateConnect(client);
+    }
+
+    private void DispatchN(Identifier identifier, int n)
+    {
+        for (int i = 0; i < n; i++)
+            Dispatch(identifier);
+    }
+
+    private Intercept Dispatch(Identifier identifier) => Dispatch(new Packet(Messages.Resolve(identifier)));
+
+    private Intercept Dispatch(IPacket packet)
+    {
+        Intercept intercept = new(Ext, packet);
+        Dispatcher.Dispatch(intercept);
+        return intercept;
     }
 
     [Fact(DisplayName = "Intercept should be correctly routed to registered handler")]
     public void TestIntercept()
     {
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        var handler = new Mock<Action<Intercept>>();
 
-        var registration = Ext.Intercept(Out.MoveAvatar, mockHandler.Object);
+        var registration = Ext.Intercept(In.Chat, handler.Object);
 
-        var packet = new Packet(Messages.Resolve(Out.MoveAvatar));
-        var intercept = new Intercept(Ext, packet);
-
-        Dispatcher.Dispatch(intercept);
+        Dispatch(In.Chat);
 
         registration.Dispose();
 
-        Dispatcher.Dispatch(intercept);
-        Dispatcher.Dispatch(intercept);
+        Dispatch(In.Chat);
 
         // Verify that the handler was invoked only once, and not after the handler was removed.
-        mockHandler.Verify(e => e.Invoke(It.IsAny<Intercept>()), Times.Once);
+        handler.Verify(e => e.Invoke(It.IsAny<Intercept>()), Times.Once);
     }
 
     [Fact(DisplayName = "Intercepting on Header.All should intercept all packets")]
@@ -72,10 +110,9 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
     {
         const int expectedCount = 5;
 
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        var handler = new Mock<Action<Intercept>>();
 
-        var registration = Ext.Intercept(Header.All, mockHandler.Object);
+        var registration = Ext.Intercept(Header.All, handler.Object);
 
         for (int i = 0; i < expectedCount; i++)
             Dispatcher.Dispatch(new(Ext, new Packet((ClientType.Flash, Direction.In, (short)i))));
@@ -85,23 +122,15 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
         for (int i = 0; i < expectedCount; i++)
             Dispatcher.Dispatch(new(Ext, new Packet((ClientType.Flash, Direction.In, (short)i))));
 
-        // Verify that the handler was invoked only once, and not after the handler was removed.
-        mockHandler.Verify(e => e.Invoke(It.IsAny<Intercept>()), Times.Exactly(expectedCount));
+        handler.Verify(e => e.Invoke(It.IsAny<Intercept>()), Times.Exactly(expectedCount));
     }
 
     [Fact(DisplayName = "Intercept successfully flags a packet as blocked")]
     public void TestBlock()
     {
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        var handler = new Mock<Action<Intercept>>();
 
-        var mockInterceptor = new Mock<IInterceptor>();
-        mockInterceptor.Setup(x => x.Dispatcher).Returns(Dispatcher);
-        mockInterceptor.Setup(x => x.Messages).Returns(Messages);
-
-        var ext = mockInterceptor.Object;
-
-        ext.Intercept([In.Chat, In.Shout, In.Whisper], e => {
+        Ext.Intercept([In.Chat, In.Shout, In.Whisper], e => {
             string msg = e.Packet.Read<string>();
             if (msg.Contains("block me"))
                 e.Block();
@@ -110,35 +139,26 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
         var pkt = new Packet(Messages.Resolve(In.Shout));
         pkt.Write("please don't block me");
 
-        var intercept = new Intercept(ext, pkt);
-        Dispatcher.Dispatch(intercept);
+        var intercept = Dispatch(pkt);
 
         Assert.True(intercept.IsBlocked);
     }
-
 
     [Fact(DisplayName = "Modify correctly modifies packet inside an intercept handler")]
     public void TestModify()
     {
         var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
 
-        var mockInterceptor = new Mock<IInterceptor>();
-        mockInterceptor.Setup(x => x.Dispatcher).Returns(Dispatcher);
-        mockInterceptor.Setup(x => x.Messages).Returns(Messages);
-
-        var ext = mockInterceptor.Object;
-
-        ext.Intercept([In.Chat, In.Shout, In.Whisper], e => {
+        Ext.Intercept([In.Chat, In.Shout, In.Whisper], e => {
             e.Packet.Read<int>();
             e.Packet.Modify<string>(s => s.Replace("apple", "orange"));
         });
 
-        var pkt = new Packet(ext.Messages.Resolve(In.Shout));
+        IPacket pkt = new Packet(Messages.Resolve(In.Shout));
         pkt.Write(1234, "I like apples");
 
-        var intercept = new Intercept(ext, pkt);
-        ext.Dispatcher.Dispatch(intercept);
+        var intercept = Dispatch(pkt);
+        pkt = intercept.Packet;
 
         pkt.Position = 0;
         pkt.Read<int>();
@@ -149,58 +169,137 @@ public class DispatcherTests : IClassFixture<MessagesFixture>
     [Fact(DisplayName = "Dispatcher.Reset should clear intercept handleres")]
     public void TestDispatcherReset()
     {
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        var handler = new Mock<Action<Intercept>>();
 
-        Ext.Intercept(Out.MoveAvatar, mockHandler.Object);
+        Ext.Intercept(In.Chat, handler.Object);
 
-        var pkt = new Packet(Messages.Resolve(Out.MoveAvatar));
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        Dispatch(In.Chat);
 
         Dispatcher.Reset();
 
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        Dispatch(In.Chat);
 
-        mockHandler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Exactly(1));
+        handler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Once);
     }
 
-    [Fact(DisplayName = "Persistent intercept should be reattached on reload of messages")]
+    [Fact(DisplayName = "Persistent intercept should be reattached on reconnect")]
     public void TestPersistentIntercept()
     {
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        var handler = new Mock<Action<Intercept>>();
 
-        Ext.Intercept(Out.MoveAvatar, mockHandler.Object);
+        Ext.Intercept(In.Chat, handler.Object);
 
-        var pkt = new Packet(Messages.Resolve(Out.MoveAvatar));
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        DispatchN(In.Chat, 1); // o
 
-        Dispatcher.Reset();
-        _fixture.LoadMessages();
+        SimulateDisconnect();
 
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        DispatchN(In.Chat, 2); // x
 
-        mockHandler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Exactly(2));
+        SimulateConnect();
+
+        DispatchN(In.Chat, 4); // o
+
+        handler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Exactly(5));
     }
 
-    [Fact(DisplayName = "Transient intercept should not persist after reload of messages")]
+    [Fact(DisplayName = "Persistent intercept should detach immediately on disposal")]
+    public void TestPersistentInterceptDisposable()
+    {
+        var handler = new Mock<Action<Intercept>>();
+
+        var persistentIntercept = Ext.Intercept(In.Chat, handler.Object);
+
+        DispatchN(In.Chat, 1); // o
+
+        persistentIntercept.Dispose();
+
+        DispatchN(In.Chat, 2); // x
+
+        SimulateDisconnect();
+
+        DispatchN(In.Chat, 4); // x
+
+        SimulateConnect();
+
+        DispatchN(In.Chat, 8); // x
+
+        handler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Once);
+    }
+
+    [Fact(DisplayName = "Transient intercept should not persist after reconnect")]
     public void TestTransientIntercept()
     {
-        var mockHandler = new Mock<Action<Intercept>>();
-        mockHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+        SimulateConnect(ClientType.Flash);
+
+        var handler = new Mock<Action<Intercept>>();
 
         Dispatcher.Register(new InterceptGroup([
-            new InterceptHandler(Out.MoveAvatar, mockHandler.Object)
+            new InterceptHandler(In.Chat, handler.Object)
         ]) { Transient = true });
 
-        var pkt = new Packet(Messages.Resolve(Out.MoveAvatar));
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        DispatchN(In.Chat, 1);
 
-        Dispatcher.Reset();
-        _fixture.LoadMessages();
+        SimulateReconnect();
 
-        Dispatcher.Dispatch(new Intercept(Ext, pkt));
+        DispatchN(In.Chat, 2);
 
-        mockHandler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Exactly(1));
+        handler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Once);
+    }
+
+    [Fact(DisplayName = "Unresolved required intercept should throw UnresolvedIdentifiersException")]
+    public void TestRequiredIntercept()
+    {
+        SimulateConnect(ClientType.Flash);
+        Assert.Throws<UnresolvedIdentifiersException>(() => {
+            Dispatcher.Register(new InterceptGroup([
+                new InterceptHandler(In.Chat, Mock.Of<Action<Intercept>>()),
+                new InterceptHandler(In.Unresolved, Mock.Of<Action<Intercept>>()) {
+                    Required = ClientType.Flash,
+                },
+            ]));
+        });
+    }
+
+    [Fact(DisplayName = "Unresolved non-required intercept should be ignored")]
+    public void TestUnrequiredIntercept()
+    {
+        SimulateConnect(ClientType.Flash);
+
+        var handler = new Mock<Action<Intercept>>();
+
+        Dispatcher.Register(new InterceptGroup([
+            new InterceptHandler(In.Chat, handler.Object),
+            new InterceptHandler(In.Unresolved, handler.Object) { Required = ClientType.None },
+            new InterceptHandler(In.Unresolved, handler.Object) { Required = ClientType.Shockwave },
+        ]));
+
+        Dispatch(In.Chat);
+
+        handler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Once);
+    }
+
+    [Fact(DisplayName = "Only targeted handlers should be attached")]
+    public void TestTargetedIntercept()
+    {
+        SimulateConnect(ClientType.Flash);
+
+        var targetedHandler = new Mock<Action<Intercept>>();
+        targetedHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+
+        var untargetedHandler = new Mock<Action<Intercept>>();
+        untargetedHandler.Setup(x => x.Invoke(It.IsAny<Intercept>()));
+
+        Dispatcher.Register(new InterceptGroup([
+            new InterceptHandler(In.Chat, targetedHandler.Object) { Target = ClientType.Flash },
+            new InterceptHandler(In.Shout, targetedHandler.Object) { Target = ClientType.Flash | ClientType.Shockwave },
+            new InterceptHandler(In.Whisper, untargetedHandler.Object) { Target = ClientType.Unity },
+        ]));
+
+        Dispatch(In.Chat);
+        Dispatch(In.Shout);
+        Dispatch(In.Whisper);
+
+        targetedHandler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Exactly(2));
+        untargetedHandler.Verify(x => x.Invoke(It.IsAny<Intercept>()), Times.Never);
     }
 }
