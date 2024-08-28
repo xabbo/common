69e05e0e0d32a41ae1851af0581f1ac1f685d2bf
rob <rob@b7c.io>
2024-08-28 14:59:13 +1200

    remove IManyParser/Composer

--- src/Xabbo.Common/Messages/IManyComposer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Xabbo.Messages;
-
-public interface IManyComposer<T> where T : IComposer, IManyComposer<T>
-{
-    static abstract void ComposeAll(in PacketWriter p, IEnumerable<T> items);
-}
