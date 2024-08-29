3c4ecb9c5fb1f4206a5dff30043a6ee852eea0d2
rob <rob@b7c.io>
2024-08-30 00:15:52 +1200

    adjust documentation

--- src/Xabbo.Common/Messages/IComposer.cs
+++ src/Xabbo.Common/Messages/IComposer.cs
@@ -1,7 +1,7 @@
 namespace Xabbo.Messages;
 
 /// <summary>
-/// Represents an object that can be serialized to a packet.
+/// Represents an object that can be composed to a packet.
 /// </summary>
 public interface IComposer
 {
