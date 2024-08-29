bacf8c2b0249e59127e49b9e207fcadb34c38a45
rob <rob@b7c.io>
2024-08-30 00:16:17 +1200

    add IParserComposer<T> interface

---
+++ src/Xabbo.Common/Messages/IParserComposer.cs
@@ -0,0 +1,6 @@
+namespace Xabbo.Messages;
+
+/// <summary>
+/// Represents an object that can be parsed from and composed to a packet.
+/// </summary>
+public interface IParserComposer<T> : IComposer, IParser<T> where T : IParser<T> { }
