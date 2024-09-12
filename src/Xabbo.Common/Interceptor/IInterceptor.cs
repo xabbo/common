e8d11f71980a39c7680cb54b8a9cd5609cec9d1a
rob <rob@b7c.io>
2024-09-12 16:09:10 +1200

    extend IInterceptorContext in IInterceptor

--- src/Xabbo.Common/Interceptor/IInterceptor.cs
+++ src/Xabbo.Common/Interceptor/IInterceptor.cs
@@ -7,10 +7,12 @@ namespace Xabbo.Interceptor;
 /// <summary>
 /// Represents a packet interceptor that can read, modify and send packets.
 /// </summary>
-public interface IInterceptor : IConnection
+public interface IInterceptor : IConnection, IInterceptorContext
 {
     /// <summary>
     /// Gets the message dispatcher associated with this interceptor.
     /// </summary>
     IMessageDispatcher Dispatcher { get; }
+
+    IInterceptor IInterceptorContext.Interceptor => this;
 }
