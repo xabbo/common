05260f9a0369398ef2b9749406f383dbbbe2711e
rob <rob@b7c.io>
2024-09-21 12:28:14 +1200

    clean up

--- src/Xabbo.Common/Messages/IRequestMessage.cs
+++ src/Xabbo.Common/Messages/IRequestMessage.cs
@@ -1,22 +1,12 @@
 namespace Xabbo.Messages;
 
-public interface IRequestMessage<TReq, TRes, TData> :
-        IMessage<TReq>,
-        IRequestFor<TRes>,
-        IResponseData<TRes, TData>
-    where
-        TReq : IMessage<TReq>
-    where
-        TRes : IMessage<TRes>;
+public interface IRequestMessage<TReq, TRes, TData> : IMessage<TReq>, IRequestFor<TRes>, IResponseData<TRes, TData>
+    where TReq : IMessage<TReq>
+    where TRes : IMessage<TRes>;
 
-public interface IRequestMessage<TReq, TRes> :
-        IMessage<TReq>,
-        IRequestFor<TRes>,
-        IResponseData<TRes, TRes>
-    where
-        TReq : IMessage<TReq>
-    where
-        TRes : IMessage<TRes>
+public interface IRequestMessage<TReq, TRes> : IMessage<TReq>, IRequestFor<TRes>, IResponseData<TRes, TRes>
+    where TReq : IMessage<TReq>
+    where TRes : IMessage<TRes>
 {
     TRes IResponseData<TRes, TRes>.GetData(TRes msg) => msg;
 }
