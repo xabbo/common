c5dc0ca7ee5a3e6a154948048c118a67510e8008
rob <rob@b7c.io>
2024-09-30 03:02:28 +1300

    remove Is... properties from Session

--- src/Xabbo.Common/Session.cs
+++ src/Xabbo.Common/Session.cs
@@ -21,33 +21,6 @@ public sealed record Session(Hotel Hotel, Client Client)
     /// <returns><c>true</c> if the specified client type contains the current session's client.</returns>
     public bool Is(ClientType client) => (Client.Type & client) != 0;
 
-    /// <summary>
-    /// Gets whether this is a Unity client session.
-    /// </summary>
-    public bool IsUnity => Client.Type is ClientType.Unity;
-
-    /// <summary>
-    /// Gets whether this is a Flash client session.
-    /// </summary>
-    public bool IsFlash => Client.Type is ClientType.Flash;
-
-    /// <summary>
-    /// Gets whether this is a Shockwave client session.
-    /// </summary>
-    public bool IsShockwave => Client.Type is ClientType.Shockwave;
-
-    /// <summary>
-    /// Gets whether this is an Origins client session.
-    /// Alias for IsShockwave.
-    /// </summary>
-    public bool IsOrigins => Client.Type is ClientType.Shockwave;
-
-    /// <summary>
-    /// Gets whether this is a modern client session.
-    /// Alias for !IsShockwave.
-    /// </summary>
-    public bool IsModern => Client.Type is not ClientType.Shockwave;
-
     public static bool operator ==(Session session, ClientType client) => session.Client.Type == client;
     public static bool operator !=(Session session, ClientType client) => !(session == client);
 }
