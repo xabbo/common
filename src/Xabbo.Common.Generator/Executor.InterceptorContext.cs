8ff88e156593f4050a5ab2b8b1774118eb9a896d
rob <rob@b7c.io>
2024-10-26 17:56:37 +1300

    fix inheritdoc references in InterceptorContext generator

--- src/Xabbo.Common.Generator/Executor.InterceptorContext.cs
+++ src/Xabbo.Common.Generator/Executor.InterceptorContext.cs
@@ -8,20 +8,11 @@ namespace Xabbo.Common.Generator;
 internal static partial class Executor
 {
     const string ContextBase = @"
-        /// <inheritdoc cref=""global::Xabbo.ConnectionExtensions.Send(
-        ///     global::Xabbo.Interceptor.IInterceptor,
-        ///     global::Xabbo.Messages.IMessage
-        /// )"" />
+        /// <inheritdoc cref=""global::Xabbo.ConnectionExtensions.Send(global::Xabbo.Connection.IConnection, global::Xabbo.Messages.IMessage)"" />
         private void Send(global::Xabbo.Messages.IMessage message)
             => global::Xabbo.ConnectionExtensions.Send(((global::Xabbo.Interceptor.IInterceptorContext)this).Interceptor, message);
 
-        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync(
-        ///     global::Xabbo.Interceptor.IInterceptor,
-        ///     global::System.ReadOnlySpan{global::Xabbo.Messages.Header},
-        ///     int, bool,
-        ///     global::System.Func{global::Xabbo.Messages.IPacket, bool}?,
-        ///     global::System.Threading.CancellationToken
-        /// )"" />
+        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync(global::Xabbo.Interceptor.IInterceptor, global::System.ReadOnlySpan{global::Xabbo.Messages.Header}, int?, bool, global::System.Func{global::Xabbo.Messages.IPacket, bool}?, global::System.Threading.CancellationToken)"" />
         private global::System.Threading.Tasks.Task<global::Xabbo.Messages.IPacket> ReceiveAsync(
             global::System.ReadOnlySpan<global::Xabbo.Messages.Header> headers,
             int? timeout = null, bool block = false,
@@ -32,13 +23,7 @@ internal static partial class Executor
             headers, timeout, block, shouldCapture, cancellationToken
         );
 
-        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync(
-        ///     global::Xabbo.Interceptor.IInterceptor,
-        ///     global::System.ReadOnlySpan{global::Xabbo.Messages.Identifier},
-        ///     int, bool,
-        ///     global::System.Func{global::Xabbo.Messages.IPacket, bool}?,
-        ///     global::System.Threading.CancellationToken
-        /// )"" />
+        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync(global::Xabbo.Interceptor.IInterceptor, global::System.ReadOnlySpan{global::Xabbo.Messages.Identifier}, int?, bool, global::System.Func{global::Xabbo.Messages.IPacket, bool}?, global::System.Threading.CancellationToken)"" />
         private global::System.Threading.Tasks.Task<global::Xabbo.Messages.IPacket> ReceiveAsync(
             global::System.ReadOnlySpan<global::Xabbo.Messages.Identifier> identifiers,
             int? timeout = null, bool block = false,
@@ -49,12 +34,7 @@ internal static partial class Executor
             identifiers, timeout, block, shouldCapture, cancellationToken
         );
 
-        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync{TMsg}(
-        ///     global::Xabbo.Interceptor.IInterceptor,
-        ///     int, bool,
-        ///     global::System.Func{TMsg, bool}?,
-        ///     global::System.Threading.CancellationToken
-        /// )"" />
+        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.ReceiveAsync{TMsg}(global::Xabbo.Interceptor.IInterceptor, int?, bool, global::System.Func{TMsg, bool}?, global::System.Threading.CancellationToken)"" />
         private global::System.Threading.Tasks.Task<TMsg> ReceiveAsync<TMsg>(
             int? timeout = null, bool block = false,
             global::System.Func<TMsg, bool>? shouldCapture = null,
@@ -64,12 +44,7 @@ internal static partial class Executor
             timeout, block, shouldCapture, cancellationToken
         );
 
-        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.RequestAsync{TReq, TRes, TData}(
-        ///     global::Xabbo.Interceptor.IInterceptor,
-        ///     global::Xabbo.Messages.IRequestMessage{TReq, TRes, TData},
-        ///     int,
-        ///     global::System.Threading.CancellationToken
-        /// )"" />
+        /// <inheritdoc cref=""global::Xabbo.InterceptorExtensions.RequestAsync{TReq, TRes, TData}(global::Xabbo.Interceptor.IInterceptor, global::Xabbo.Messages.IRequestMessage{TReq, TRes, TData}, int?, bool, global::System.Threading.CancellationToken)"" />
         private async global::System.Threading.Tasks.Task<TData> RequestAsync<TReq, TRes, TData>(
             global::Xabbo.Messages.IRequestMessage<TReq, TRes, TData> request,
             int? timeout = null, global::System.Threading.CancellationToken cancellationToken = default
